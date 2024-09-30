8e66cdc2882c29643275687cbe9a775bc613e3a7
Nova <168435550+novakoolhub@users.noreply.github.com>
2024-09-30 21:06:04 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+# Libraries
+
+from flask import Flask, request
+import requests
+import json
+
+from PIL import PilImage
+from io import BytesIO
+
+
+# Functions
+
+def clamp(Number, Min, Max):
+    if Number < Min:
+        return Min
+    elif Number > Max: 
+        return Max
+    else: 
+        return Number 
+
+
+# App
+
+App = Flask(__name__)
+
+@App.route('/')
+def Post():
+    ImageURL = request.args.get("URL")
+    ImageSizeX = int(request.args.get("SizeX"))
+    ImageSizeY = int(request.args.get("SizeY"))
+    ImageSize = [clamp(ImageSizeX, 10, 240), clamp(ImageSizeY, 10, 240)]
+    
+    Response = requests.get(ImageURL)
+    NewImage = PilImage.open(BytesIO(Response.content)).resize(ImageSize)
+    Data = list(NewImage.getdata())
+
+    return json.dumps({
+            "Data": Data,
+            "Size": NewImage.size,
+        })
+
+App.run()
